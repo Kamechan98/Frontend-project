3647431d471f5de28fbe280cc472ba8fc72cff2e
Evelina Nyman <ina.nyman98@gmail.com>
2023-10-23 17:36:19 +0200

    first additions

--- frontend-project/src/App.tsx
+++ frontend-project/src/App.tsx
@@ -7,7 +7,12 @@ const App = () => {
     <div>
       <Nav/>
       <div className='div'>
-        dfghjklö
+        <h1>NorthernNest Retreat</h1>
+        <h2>Explore our luxury & romantic cabins for couples</h2>
+        <p>Select one of our generous packages, along with a modern, comfy and luxurious decor 
+        where you and your partner can spend some romatic time with each other, whether it be by
+        skiing, hiking, or by taking a warm bath or spend an hour in the sauna.
+        </p>
       </div>
       <Footer/>
     </div>
