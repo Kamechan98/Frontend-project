3c61f55a3f06abd96060d8841086afce094ed964
Evelina Nyman <ina.nyman98@gmail.com>
2023-10-23 17:24:36 +0200

    first additions

---
+++ frontend-project/src/components/Nav/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nav.scss'
+
+const Nav = () => {
+  return (
+    <div>
+        <nav className='nav-bar' id='nav-bar'>
+            Nav
+            {/* <img src="assets/Logo.svg" alt="logo" /> */}
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
